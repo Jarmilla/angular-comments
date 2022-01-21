55d573532fd0bb0edf70c704806e055397b34ed7
Jarmilla <bodorne.deme.adrienn@gmail.com>
2022-01-21 15:46:25 +0100

    getting data from API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {CommentService} from "./services/comment.service"
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-comments';
+
+  constructor(private comment : CommentService) {}
+
+  getDataFromAPI() {
+    this.comment.getData().subscribe((data) => console.log(data))
+  }
 }
